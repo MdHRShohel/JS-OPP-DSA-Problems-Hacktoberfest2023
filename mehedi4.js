4f72f6b5223aefc065ff0581f07bfa279ef514d7
Md. Mehedi Hasan <106642643+0mehedihasan@users.noreply.github.com>
2023-10-01 01:50:17 +0600

    Create mehedi4.js

---
+++ mehedi4.js
@@ -0,0 +1,17 @@
+let grid = document.querySelector(".grid");
+let popup = document.querySelector(".popup");
+let playAgain = document.querySelector(".playAgain");
+let scoreDisplay = document.querySelector(".scoreDisplay");
+let left = document.querySelector(".left");
+let bottom = document.querySelector(".bottom");
+let right = document.querySelector(".right");
+let up = document.querySelector(".top");
+let width = 10;
+let currentIndex = 0;
+let appleIndex = 0;
+let currentSnake = [2, 1, 0];
+let direction = 1;
+let score = 0;
+let speed = 0.8;
+let intervalTime = 0;
+let interval = 0;
