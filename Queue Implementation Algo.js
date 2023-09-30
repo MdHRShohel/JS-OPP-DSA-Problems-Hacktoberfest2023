d84a89b8501f09bff21f47e3f38b12f541fc962d
MAHAMUDUL HASAN MITHUN <113894569+MHMITHUN@users.noreply.github.com>
2023-10-01 01:38:03 +0600

    Create Queue Implementation Algo.js

---
+++ Queue Implementation Algo.js
@@ -0,0 +1,23 @@
+class Queue {
+    constructor() {
+        this.items = [];
+    }
+
+    enqueue(element) {
+        this.items.push(element);
+    }
+
+    dequeue() {
+        if (this.isEmpty()) return "Underflow";
+        return this.items.shift();
+    }
+
+    front() {
+        if (this.isEmpty()) return "No elements in Queue";
+        return this.items[0];
+    }
+
+    isEmpty() {
+        return this.items.length === 0;
+    }
+}
