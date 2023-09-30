d47c5ca58cf7f94f9d00fec3d19cd5a1377eb411
MAHAMUDUL HASAN MITHUN <113894569+MHMITHUN@users.noreply.github.com>
2023-10-01 01:30:08 +0600

    Create Linked List Implementation.js

---
+++ Linked List Implementation.js
@@ -0,0 +1,27 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    addNode(data) {
+        const newNode = new Node(data);
+
+        if (this.head === null) {
+            this.head = newNode;
+        } else {
+            let current = this.head;
+            while (current.next !== null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+    
+}
