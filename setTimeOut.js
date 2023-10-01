456d426eceb6e35a0e5bb69e594b54bb9748168f
Shubh011 <singhshubhanjali01@gmail.com>
2023-10-01 23:11:56 +0530

    setTimeOut

---
+++ setTimeOut.js
@@ -0,0 +1,12 @@
+const number1 = 10;
+const number2 = 20;
+const number3 = 30;
+const number4 = 40;
+const number5 = 50;
+console.log(number1);
+console.log(number2);
+setTimeout(function () {
+  console.log(number3);
+}, 2000);
+console.log(number4);
+console.log(number5);
