8a24a80c72c045750020374a8830e70baf5b3640
Sanjana Ayshi <sanjana.ayshi92@gmail.com>
2023-10-01 19:00:49 +0600

    max number in js updated!

---
+++ maxNumber.js
@@ -0,0 +1,16 @@
+function max_of_three(x, y, z) {
+  max_val = 0;
+  if (x > y) {
+    max_val = x;
+  } else {
+    max_val = y;
+  }
+  if (z > max_val) {
+    max_val = z;
+  }
+  return max_val;
+}
+
+console.log(max_of_three(1, 0, 1));
+console.log(max_of_three(0, -10, -20));
+console.log(max_of_three(1000, 510, 440));
