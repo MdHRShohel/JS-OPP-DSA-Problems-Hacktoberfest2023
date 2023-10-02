533c76e098d4f18362ebff3e62dc341a7b6b7dcc
sanjana-ayshi <sanjana.ayshi92@gamil.com>
2023-10-02 20:57:03 +0600

    update trianglejs1

--- triangle.js
+++ triangle.js
@@ -1,5 +1,5 @@
-var side1 = 5;
-var side2 = 6;
+var side1 = 2;
+var side2 = 3;
 var side3 = 7;
 var s = (side1 + side2 + side3) / 2;
 var area = Math.sqrt(s * ((s - side1) * (s - side2) * (s - side3)));
