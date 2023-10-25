82f5972a3d37d64aea8d2e028a79afdf704ae6bf
Shreya <shreyaprasadaug@gmail.com>
2023-10-25 11:53:17 +0530

    added new project

---
+++ Travel website design/main.js
@@ -0,0 +1,59 @@
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+// header container
+ScrollReveal().reveal(".header__container h1", scrollRevealOption);
+
+ScrollReveal().reveal(".header__container h4", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".header__container .btn", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+
+// about container
+ScrollReveal().reveal(".about__container .section__header", scrollRevealOption);
+ScrollReveal().reveal(".about__container .section__subheader", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".about__container .about__flex", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+
+ScrollReveal().reveal(".about__container .btn", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+
+// discover container
+ScrollReveal().reveal(".discover__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+ScrollReveal().reveal(".discover__card__content", {
+  ...scrollRevealOption,
+  interval: 500,
+  delay: 200,
+});
+
+// blogs container
+ScrollReveal().reveal(".blogs__card", {
+  duration: 1000,
+  interval: 400,
+});
+
+// journals container
+ScrollReveal().reveal(".journals__card", {
+  ...scrollRevealOption,
+  interval: 400,
+});
